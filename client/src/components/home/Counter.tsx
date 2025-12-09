de4ea87c9a602aa5b14c83f29208158b09509d70
Apon Shekh <aponshekh420@gmail.com>
2025-12-09 11:25:43 +0600

    The home chanage 90% has completed

---
+++ client/src/components/home/Counter.tsx
@@ -0,0 +1,40 @@
+const Counter = () => {
+  return (
+    <div className="mt-10">
+      <div className="container">
+        <div className="grid lg:grid-cols-4 grid-cols-2 gap-3 gap-y-15">
+
+          {/* grid 1 */}
+          <div className="flex items-center flex-col">
+            <p className="text-teal font-bold text-5xl">6,00</p>
+            <p className="font-semibold text-teal text-lg mt-2 text-center">Couples</p>
+            <p className="text-md text-[#333333] text-center">Who really want to know!</p>
+          </div>
+
+          {/* grid 2 */}
+          <div className="flex items-center flex-col">
+            <p className="text-teal font-bold text-5xl">15</p>
+            <p className="font-semibold text-teal text-lg mt-2 text-center">Courses</p>
+            <p className="text-md text-[#333333] text-center">Comprehensive In-depth information</p>
+          </div>
+
+          {/* grid 3 */}
+          <div className="flex items-center flex-col">
+            <p className="text-teal font-bold text-5xl">45</p>
+            <p className="font-semibold text-teal text-lg mt-2 text-center">Speakers</p>
+            <p className="text-md text-[#333333] text-center">Professionals in the field</p>
+          </div>
+
+          {/* grid 4 */}
+          <div className="flex items-center flex-col">
+            <p className="text-teal font-bold text-5xl">90</p>
+            <p className="font-semibold text-teal text-lg mt-2 text-center">Lectures</p>
+            <p className="text-md text-[#333333] text-center">Answers to questions you’re embarrassed to ask.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
--- client/src/components/home/CoupleFaced.tsx
+++ client/src/components/home/CoupleFaced.tsx
@@ -5,38 +5,6 @@ import FadeInSection from "../common/FadeInSection";
 export default function CoupleFaced() {
   return (
     <section className="mt-20">
-      <div className="container my-20">
-        <div className="text-center max-w-[1080px] mx-auto">
-          <FadeInSection
-            initial={{ opacity: 0, y: 50 }}
-            scrollTop={{ opacity: 1, y: 0 }}
-            scrollBottom={{ opacity: 0, y: 50 }}
-            margin="40px 0px -40px 0px"
-          > 
-            <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl">
-              You want a home filled with peace, connection, and holiness.
-            </h1>
-          </FadeInSection>
-          <FadeInSection
-            initial={{ opacity: 0, y: 50 }}
-            scrollTop={{ opacity: 1, y: 0 }}
-            scrollBottom={{ opacity: 0, y: 50 }}
-            margin="40px 0px -40px 0px"
-            delay={0.5}
-          > 
-            <p className="mt-4 mb-14 text-base lg:text-lg">
-              But when challenges arise in marriage — especially in the private areas of life — it can feel overwhelming.
-              Many couples don&apos;t know where to turn for discreet, Torah-true guidance.  
-              <b>Ohel Miriam was created to address knowledge gaps on the 
-              Torah perspective of marital harmony, intimate relationships and related topics.</b>
-              Ohel Miriam helps Jewish couples build harmony and intimacy in marriage
-              through trusted guidance, practical tools, and Torah-aligned wisdom.
-              Ohel Miriam gives you the tools and knowledge to build a marriage that truly thrives. 
-              Ohel Miriam arranges live teleconferences and courses, catering to both men and women alike. These sessions feature insights from Rabbanim, women lecturers, therapists and medical professionals.
-            </p>
-          </FadeInSection>
-        </div>
-      </div>
       <div className="bg-ligtGray">
         <div className="container py-14">
           <div className="text-center">
