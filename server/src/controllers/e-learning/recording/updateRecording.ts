640776a073f6f6d65d2230dcbf1ef086a2efab32
shipon islam <shiponkhan566@gmail.com>
2026-04-14 14:19:22 +0600

    fixed some issues of recording page

--- server/src/controllers/e-learning/recording/updateRecording.ts
+++ server/src/controllers/e-learning/recording/updateRecording.ts
@@ -32,31 +32,46 @@ export const updateRecording = async (
     const body = req.body;
     const deletedImages = body.deletedImages ? body.deletedImages : [];
     const id = req.params?.id;
+
+    const existingRecordings =
+      JSON.parse(body.existingRecordings).length > 0
+        ? JSON.parse(body.existingRecordings)
+        : [];
+
     const recordingsObj: CreateRecordingBody = {
       heading: body.heading,
       recordingCategory: body.recordingCategory,
+      recordings: existingRecordings,
     };
     // FormData usually sends array as string
     const parsedRecordings: RecordingBody[] =
       typeof body.recordings === "string"
         ? JSON.parse(body.recordings)
         : body.recordings;
+
     if (parsedRecordings && parsedRecordings.length > 0 && files.length > 0) {
-      recordingsObj.recordings = parsedRecordings.map((rec, index) => ({
-        ...rec,
-        file: files[index]?.filename,
-      }));
+      let index = 0;
+      const newRecordings = parsedRecordings.map((rec) => {
+        if (rec.externalLink) {
+          return rec;
+        } else {
+          const fileWithRec = {
+            ...rec,
+            file: files[index]?.filename || null,
+          };
+          index++;
+          return fileWithRec;
+        }
+      });
+      recordingsObj.recordings = [...newRecordings, existingRecordings];
     }
-    if (parsedRecordings && parsedRecordings.length > 0) {
-      recordingsObj.recordings = parsedRecordings.map((rec, index) => ({
-        ...rec,
-      }));
+    if (parsedRecordings && parsedRecordings.length > 0 && files.length <= 0) {
+      recordingsObj.recordings = [...parsedRecordings, ...existingRecordings];
     }
     if (body.gender) recordingsObj.gender = body.gender;
     if (body.speakerId) recordingsObj.speaker = body.speakerId;
     if (body.courseId) recordingsObj.course = body.courseId;
     if (body.classId) recordingsObj.class = body.classId;
-
     const recordingData = await RecordingModel.findByIdAndUpdate(
       id,
       recordingsObj,
