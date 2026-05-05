9b4839fc2077d38e6b47a510395aa92aa6377a2e
Apon Shekh <aponshekh420@gmail.com>
2026-05-05 12:21:45 +0600

    again i added change on hero section

--- client/src/components/dashboard/ecommerce/products/ProductForm.tsx
+++ client/src/components/dashboard/ecommerce/products/ProductForm.tsx
@@ -636,7 +636,7 @@ export default function CreateProductForm({
                     dispatch(addProductField({ shippingClass: val }))
                   }
                   options={[
-                    { label: "noneee", value: "nonee" },
+                    { label: "none", value: "none" },
                     { label: "My Shemen Class", value: "my-shemen-class" },
                     { label: "1.5 lb Class", value: "1.5-lb-class" },
                     { label: "Ebook Class", value: "ebook-class" },
