80654d2380e525e87a186c53d100a26727df73b7
shipon islam <shiponkhan566@gmail.com>
2026-06-16 11:07:18 +0600

    created resources and about page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,30 +1,48 @@
 import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 import Wrapper from "@/components/common/wrapper";
 import ReduxStoreProvider from "@/context-provider/ReduxStoreProvider";
 import type { Metadata } from "next";
-import { Inter, Roboto, Roboto_Slab } from "next/font/google";
+import {
+  Inter,
+  Lexend_Deca,
+  Lora,
+  Montserrat,
+  Playfair_Display,
+  Roboto,
+  Roboto_Slab,
+} from "next/font/google";
 import { Toaster } from "react-hot-toast";
-
-
-export const metadata: Metadata = {
-  title: 'Ohel Miriam',
-  description: `Ohel Miriam strengthens couples in building marriages grounded in kedusha, shalom, and simcha. Through this journey, you'll gain four essential gifts for deeper`,
-  icons: {
-    icon: '/logo.png',
-    shortcut: '/logo.png',
-    apple: '/logo.png',
-  },
-}
-
-
-import Header from "@/components/common/Header";
 import "swiper/css";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Ohel Miriam",
+  description: `Ohel Miriam strengthens couples in building marriages grounded in kedusha, shalom, and simcha. Through this journey, you'll gain four essential gifts for deeper`,
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
+};
+
+export const lexend_deca = Lexend_Deca({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-lexend-deca",
+});
 export const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
+});
+export const playfair_display = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-playfair-display",
 });
 export const roboto = Roboto({
   subsets: ["latin"],
@@ -38,6 +56,17 @@ export const robotoSlab = Roboto_Slab({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-roboto-slab",
 });
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
+export const lora = Lora({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lora",
+});
 
 export default function RootLayout({
   children,
@@ -45,11 +74,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className}  antialiased`}
-        suppressHydrationWarning
-      >
+    <html
+      lang="en"
+      className={`${inter.variable} ${lexend_deca.variable} ${lora.variable}  ${montserrat.variable} ${roboto.variable} ${robotoSlab.variable} ${playfair_display.variable}`}
+      suppressHydrationWarning
+    >
+      <body className={` antialiased`} suppressHydrationWarning>
         <Toaster />
         <ReduxStoreProvider>
           <Wrapper>
