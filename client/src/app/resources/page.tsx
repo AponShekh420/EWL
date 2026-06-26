5b7c03dd94298c64b0f93173db1140b46660e4ef
Apon Shekh <aponshekh420@gmail.com>
2026-06-27 01:01:18 +0600

    The resourse has edited for fix error

--- client/src/app/resources/page.tsx
+++ client/src/app/resources/page.tsx
@@ -89,10 +89,21 @@ export default async function Resources({
 }) {
   const q = await searchParams;
   const query = await queryFormatter(searchParams);
-  const [resources, categories] = await Promise.all([
-    getResourcesByQueryWithVisible(query),
-    getResourcesCategories(),
-  ]);
+  let resources;
+  let categories;
+
+  try {
+      [resources, categories] = await Promise.all([
+          getResourcesByQueryWithVisible(query),
+          getResourcesCategories(),
+      ]);
+
+      console.log(resources);
+      console.log(categories);
+  } catch (err) {
+      console.error(err);
+      throw err;
+  }
   return (
     <main>
       <section className="bg-[url('/images/home/hero.png')] bg-cover bg-center bg-no-repeat bg-blend-overlay bg-black/15 min-h-100">
@@ -129,16 +140,16 @@ export default async function Resources({
               All
             </Button>
           </Link>
-          {categories?.data.map((category: BlogCategoryType) => (
+          {categories?.data?.map((category: BlogCategoryType) => (
             <Link
-              href={`/resources?category=${category.name}`}
-              key={category._id}
+              href={`/resources?category=${category?.name}`}
+              key={category?._id}
             >
               <Button
                 variant="outline"
-                className={`transform transition duration-300 ${q.category === category.name ? "text-white bg-purple-cs" : "bg-white text-purple-cs"} hover:bg-purple-cs hover:text-white capitalize`}
+                className={`transform transition duration-300 ${q.category === category?.name ? "text-white bg-purple-cs" : "bg-white text-purple-cs"} hover:bg-purple-cs hover:text-white capitalize`}
               >
-                {category.name}
+                {category?.name}
               </Button>
             </Link>
           ))}
@@ -158,25 +169,28 @@ export default async function Resources({
           </Link>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-12">
-          {resources?.data.map((resource: ResourcesType) => (
+          {resources?.data?.map((resource: ResourcesType) => (
             <div
-              key={resource._id}
+              key={resource?._id}
               className="bg-white rounded-lg shadow-[1px_1px_4px] shadow-black/20 overflow-hidden flex flex-col justify-between items-center"
             >
               <Image
-                src={getImageUrl(resource.thumbnail, "resources")}
+                src={resource.thumbnail
+                  ? getImageUrl(resource?.thumbnail, "resources")
+                  : "/images/resources/default.png"
+                }
                 alt={resource.title}
                 width={200}
                 height={200}
                 className="h-25 object-contain"
               />
               <div className="p-4 flex-1 flex flex-col text-center">
-                <h2 className="text-xl font-semibold mb-2">{resource.title}</h2>
+                <h2 className="text-xl font-semibold mb-2">{resource?.title}</h2>
                 <p className="text-gray-600 mb-4 flex-1 font-lora">
-                  {resource.description}
+                  {resource?.description}
                 </p>
                 <a
-                  href={resource.link}
+                  href={resource?.link}
                   target="_blank"
                   className="block w-full"
                 >
@@ -184,7 +198,7 @@ export default async function Resources({
                     variant="outline"
                     className={`mt-auto flex w-full font-montserrat font-semibold text-purple-cs  hover:bg-purple-cs hover:text-white transform transition duration-300 `}
                   >
-                    {resource.title}
+                    {resource?.title}
                     <Icon
                       icon="material-symbols:arrow-right-alt"
                       className="mr-2"
@@ -195,9 +209,11 @@ export default async function Resources({
             </div>
           ))}
         </div>
-        <div className="w-fit ml-auto py-8">
-          <ShopPagination pagination={resources.pagination} />
-        </div>
+        {resources?.pagination && (
+            <div className="w-fit ml-auto py-8">
+                <ShopPagination pagination={resources?.pagination} />
+            </div>
+        )}
       </section>
     </main>
   );
